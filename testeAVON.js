1adb0c78336ffee74e60d0f7a1c95754375462a7
PedroDiniz123 <pedrojunqueira5@gmail.com>
2024-08-23 16:46:21 -0300

    ws

---
+++ testeAVON.js
@@ -0,0 +1,10 @@
+let nome1 = 'Anna';
+
+{
+    let idade1 = 25;
+    console.log(nome1);  // Exibe: Anna
+    console.log(idade1); // Exibe: 25
+}
+
+console.log(nome1);  // Exibe: Anna
+console.log(idade1); // ReferenceError: idade1 is not defined
